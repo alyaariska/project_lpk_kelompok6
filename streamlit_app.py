a6c961d9bd4749938f5d20b132ad9fc8a6fa458a
alyaariska <alyaariska26@gmail.com>
2025-07-12 09:03:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,12 @@ st.header("Four", divider=True)
 import streamlit as st
 
 st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=["lat", "lon"],
+)
+st.map(df)
