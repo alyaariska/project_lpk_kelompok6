7a143c6166a60049d00a8db2d71df6a8948134e1
alyaariska <alyaariska26@gmail.com>
2025-07-12 09:00:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.write("Hello, *World!* :sunglasses:")
+import streamlit as st
+
+st.header("_Streamlit_ is :blue[cool] :sunglasses:")
+st.header("This is a header with a divider", divider="gray")
+st.header("These headers have rotating dividers", divider=True)
+st.header("One", divider=True)
+st.header("Two", divider=True)
+st.header("Three", divider=True)
+st.header("Four", divider=True)
+import streamlit as st
+
+st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
